8b1af62f730e0ee5d1408f4041ec84cb86a1235b
Torbjörn Törnkvist <kruskakli@gmail.com>
2024-10-28 20:33:21 +0100

    fixing the test_parser.py

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,55 +1,55 @@
-"""
-Test cases for the functional parser with comparison operators.
-"""
+"""Unit tests for the functional parser with comparison operators."""
 
+import unittest
 from mfl_parser import FunctionalParser
 from mfl_type_checker import BinOp, Int, Bool, infer_j
 
-def test_comparison_operators():
-    parser = FunctionalParser([], {})
+class TestComparisonOperators(unittest.TestCase):
 
-    # Test greater than
-    ast = parser.parse("5 > 3")
-    assert isinstance(ast, BinOp)
-    assert ast.op == ">"
-    assert ast.left.value == 5
-    assert ast.right.value == 3
+    def setUp(self):
+        self.parser = FunctionalParser([], {})
 
-    # Test less than
-    ast = parser.parse("2 < 7")
-    assert isinstance(ast, BinOp)
-    assert ast.op == "<"
-    assert ast.left.value == 2
-    assert ast.right.value == 7
+    def test_greater_than(self):
+        ast = self.parser.parse("5 > 3")
+        self.assertIsInstance(ast, BinOp)
+        self.assertEqual(ast.op, ">")
+        self.assertEqual(ast.left.value, 5)
+        self.assertEqual(ast.right.value, 3)
 
-    # Test equality
-    ast = parser.parse("4 == 4")
-    assert isinstance(ast, BinOp)
-    assert ast.op == "=="
-    assert ast.left.value == 4
-    assert ast.right.value == 4
+    def test_less_than(self):
+        ast = self.parser.parse("2 < 7")
+        self.assertIsInstance(ast, BinOp)
+        self.assertEqual(ast.op, "<")
+        self.assertEqual(ast.left.value, 2)
+        self.assertEqual(ast.right.value, 7)
 
-    # Test less than or equal
-    ast = parser.parse("3 <= 3")
-    assert isinstance(ast, BinOp)
-    assert ast.op == "<="
-    assert ast.left.value == 3
-    assert ast.right.value == 3
+    def test_equality(self):
+        ast = self.parser.parse("4 == 4")
+        self.assertIsInstance(ast, BinOp)
+        self.assertEqual(ast.op, "==")
+        self.assertEqual(ast.left.value, 4)
+        self.assertEqual(ast.right.value, 4)
 
-    # Test greater than or equal
-    ast = parser.parse("8 >= 5")
-    assert isinstance(ast, BinOp)
-    assert ast.op == ">="
-    assert ast.left.value == 8
-    assert ast.right.value == 5
+    def test_less_than_or_equal(self):
+        ast = self.parser.parse("3 <= 3")
+        self.assertIsInstance(ast, BinOp)
+        self.assertEqual(ast.op, "<=")
+        self.assertEqual(ast.left.value, 3)
+        self.assertEqual(ast.right.value, 3)
 
-    # Test type checking of comparison expressions
-    ctx = {}
-    for expr in [ast]:  # We could test all expressions, using last one for example
-        type_result = infer_j(expr, ctx)
-        assert str(type_result) == "bool"  # All comparison operators should return bool type
+    def test_greater_than_or_equal(self):
+        ast = self.parser.parse("8 >= 5")
+        self.assertIsInstance(ast, BinOp)
+        self.assertEqual(ast.op, ">=")
+        self.assertEqual(ast.left.value, 8)
+        self.assertEqual(ast.right.value, 5)
 
-    print("All comparison operator tests passed!")
+    def test_type_checking(self):
+        ctx = {}
+        for expr in [self.parser.parse("5 > 3"), self.parser.parse("2 < 7"), self.parser.parse("4 == 4"), self.parser.parse("3 <= 3"), self.parser.parse("8 >= 5")]:
+            type_result = infer_j(expr, ctx)
+            self.assertEqual(str(type_result), "bool")
+
 
 if __name__ == "__main__":
-    test_comparison_operators()
+    unittest.main()
