bef1e02b18f8e3684fc9f40cea92f0de35325630
Torbjörn Törnkvist <kruskakli@gmail.com>
2024-10-28 21:11:56 +0100

    Adding parser tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -3,6 +3,7 @@
 import unittest
 from mfl_parser import FunctionalParser
 from mfl_type_checker import BinOp, Int, Bool, infer_j
+from mfl_ast import execute_ast
 
 class TestComparisonOperators(unittest.TestCase):
 
@@ -49,6 +50,21 @@ class TestComparisonOperators(unittest.TestCase):
         for expr in [self.parser.parse("5 > 3"), self.parser.parse("2 < 7"), self.parser.parse("4 == 4"), self.parser.parse("3 <= 3"), self.parser.parse("8 >= 5")]:
             type_result = infer_j(expr, ctx)
             self.assertEqual(str(type_result), "bool")
+
+    def test_let_expression_double(self):
+        ast = self.parser.parse("let double = λx.(x*2) in (double 21)")
+        result = execute_ast(ast, False)
+        self.assertEqual(result, 42)
+
+    def test_let_expression_add(self):
+        ast = self.parser.parse("let add = λx.λy.(x+y) in (add 3 4)")
+        result = execute_ast(ast, False)
+        self.assertEqual(result, 7)
+
+    def test_let_expression_compose(self):
+        ast = self.parser.parse("let compose = λf.λg.λx.(f (g x)) in let add1 = λx.(x+1) in let double = λx.(x+x) in ((compose double add1) 2)")
+        result = execute_ast(ast, False)
+        self.assertEqual(result, 6)
 
 
 if __name__ == "__main__":
